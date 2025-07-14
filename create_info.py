b721e7a5b089b4972c4507cd2f24d7bf5077a07a
Zainab <shahbazahmadzainab@gmail.com>
2025-07-14 18:33:18 +0500

    Add files via upload

---
+++ create_info.py
@@ -0,0 +1,5 @@
+with open("info.txt", "w") as file:
+    file.write("I am learning Python.\n")
+    file.write("Python is very intresting.\n")
+    file.write("I love programming.\n")
+print("info.txt created successfully!")
